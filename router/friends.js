35047e0b11b6faf9ac850260caa6d54fb51d3c3e
ingarskrafts <ingarskrafts@gmail.com>
2026-05-28 06:15:05 -0400

    Exercise 4: Implement the POST method

--- router/friends.js
+++ router/friends.js
@@ -27,8 +27,17 @@ router.get("/:email",(req,res)=>{
 
 // POST request: Add a new friend
 router.post("/",(req,res)=>{
-  // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  // Check if email is provided in the request body
+  if (req.body.email) {
+    // Create or update friend's details based on provided email
+    friends[req.body.email] = {
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "DOB": req.body.DOB
+    };
+  }
+  // Send response indicating user addition
+  res.send("The user" + (' ') + (req.body.firstName) + "Has been added!");
 });
 
 
