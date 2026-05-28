c5e744e4c799aa71aff3949dd0923a3d8dc0bce6
ingarskrafts <ingarskrafts@gmail.com>
2026-05-28 05:45:53 -0400

    Exercise 2: Implement the GET method:

--- router/friends.js
+++ router/friends.js
@@ -12,9 +12,9 @@ let friends = {
 // GET request: Retrieve all friends
 router.get("/",(req,res)=>{
 
-  // Update the code here
+  // Send JSON response with formatted friends data
 
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(JSON.stringify(friends,null,4));
 });
 
 // GET by specific ID request: Retrieve a single friend with email ID
